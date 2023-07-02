2c7f51b9bd91e7674d97f3dee1ac52450d0ca190
Edwin <edwin.h.morris@gmail.com>
2023-07-02 10:00:11 -0400

    Adjust frame rate

--- helpers/constants.js
+++ helpers/constants.js
@@ -4,4 +4,4 @@ export const LANDER_HEIGHT = 40;
 export const CRASH_VELOCITY = 0.6;
 export const VELOCITY_MULTIPLIER = 20;
 export const CRASH_ANGLE = 11;
-export const INTERVAL = Math.floor(1000 / 120);
+export const INTERVAL = Math.floor(1000 / 100);
